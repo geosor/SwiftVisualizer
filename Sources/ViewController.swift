d357a2905648b3ac5c7f006d156de4e34acdba26
Søren Mortensen <soren@sorenmortensen.com>
2017-03-06 19:23:50 +0000

    Reorganise project again

---
+++ Sources/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftVisualizer
+//
+//  Created by Søren Mortensen on 06/03/2017.
+//  Copyright © 2017 Søren Mortensen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
